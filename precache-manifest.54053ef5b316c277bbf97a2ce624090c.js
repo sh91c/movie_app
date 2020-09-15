bcc200a781e5b43f5f34d3f201de92861e7ccf7c
sh91c <52781333+sh91c@users.noreply.github.com>
2020-09-15 11:17:38 +0900

    Updates

---
+++ precache-manifest.54053ef5b316c277bbf97a2ce624090c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d5928c54c58601d1d27d2aabdb265e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "fa24e6c170e66c186af6",
+    "url": "/movie_app/static/css/main.4e8e8bd1.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa24e6c170e66c186af6",
+    "url": "/movie_app/static/js/main.b6bc24d5.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
